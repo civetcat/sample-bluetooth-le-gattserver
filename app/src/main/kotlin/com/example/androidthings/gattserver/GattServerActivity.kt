f451cb76997ae913f9dee9fd504d2b2d870de9e7
Daniel <daniel.ch@mic.com.tw>
2022-08-22 13:50:31 +0800

    Single command response

--- app/src/main/kotlin/com/example/androidthings/gattserver/GattServerActivity.kt
+++ app/src/main/kotlin/com/example/androidthings/gattserver/GattServerActivity.kt
@@ -58,8 +58,8 @@ class GattServerActivity : Activity() {
             }
         }
 
-        /* 1. OPCODE(Byte 0), Reserved(Byte 1), Sequence number(index)(Byte 2), 0 -> Number of packets / 1-N -> item ID(Byte 3), String value(Byte 4-19)
-           2. Calculate total package length  = Total length / 15
+        /* 1. OPCODE(Byte 0), Reserved(Byte 1), Sequence number(index)(Byte 2), 0 -> Number of packets / 1-N -> item ID(Byte 3), String value(Byte 4-19) (OK)
+           2. Calculate total package length  = Total length / 15 (OK)
            3. Split
            4. for loop / while loop to send notify to devices who is subscribed
          */
--- app/src/main/kotlin/com/example/androidthings/gattserver/SinglePacketManager.kt
+++ app/src/main/kotlin/com/example/androidthings/gattserver/SinglePacketManager.kt
@@ -1,22 +1,27 @@
 package com.example.androidthings.gattserver
 
 object SinglePacketManager {
-
-    // open wifi hotspot?
-    fun sendWifiHotSpotPacket(sendPacket: ByteArray, index: Int, itemId: Int): ByteArray {
-        val output = ByteArray(20)
-        output[0] = 0x01
+    fun sendWifiHotSpotPacket(success: Boolean): ByteArray {
+        val output = ByteArray(3)
+        output[0] = 0x02
         output[1] = 0x00
-        output[2] = index.toByte()
-        output[3] = itemId.toByte() // item id , temp set 01 for vendor info
-        output[4] = 0x32
-        output[4] = 0x31
-
-        for (i in 4 until output.size) {
-            output[i] = sendPacket[i]
+        if (!success) {
+            output[2] = 0x00 // 0x01 Success, 0x00 Fail
+        } else {
+            output[2] = 0x01 // 0x01 Success, 0x00 Fail
         }
-
         return output
     }
 
+    fun sendInstallComplete(success: Boolean): ByteArray {
+        val output = ByteArray(3)
+        output[0] = 0x03
+        output[1] = 0x00
+        if (!success) {
+            output[2] = 0x00 // 0x01 Success, 0x00 Fail
+        } else {
+            output[2] = 0x01 // 0x01 Success, 0x00 Fail
+        }
+        return output
+    }
 }
